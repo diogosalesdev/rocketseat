d932ffcf089514a6db4c46450fa7652227c81a01
Diogo Sales <diogosalesdev@gmail.com>
2020-08-19 14:04:58 -0300

    Immutability, State and Props concepts aplied

--- GoStack/frontend/src/App.js
+++ GoStack/frontend/src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header';
 
 function App() {
+  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+  function handleAddProject() {
+    setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+  }
+
   return (
-    <>
-      <Header />
-      <Header />
+    <>      
+      <Header title="Projects" />
+      <ul>
+        {projects.map(project => <li key={project} >{project}</li> )}
+      </ul>
+      <button type="button" onClick={handleAddProject} >Adicionar projeto</button>
     </>  
   );
 }
